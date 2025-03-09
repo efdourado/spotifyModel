2c8a4765fd323298c8428f61f75f1016e202237d
efdourado <ed320819@gmail.com>
2025-03-09 20:22:29 -0300

    Minor fixes and Deploy implementation

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// const URL = "https://deploy-jornada-full-stack.onrender.com/api";
-
-const URL = "http://localhost:3003/api";
+const URL = "https://spotifymodel.onrender.com/api";
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
 
